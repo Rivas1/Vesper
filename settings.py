b65a9fa58b4ceda1d1ec7c35662e0d11a01e535b
Rivas1 <rivas1@cpp.edu>
2019-02-01 01:34:43 -0800

    Attempt to integrate Jinja2 (incomplete)
    ERROR:
    AttributeError at /vehicles/
    'dict' object has no attribute 'updates'

--- settings.py
+++ settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'vehicles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,6 +53,14 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'Vesper.urls'
 
 TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'environment': 'Vesper.jinja2.environment'
+        },
+    },
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
